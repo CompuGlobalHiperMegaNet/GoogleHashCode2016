d74bd3dbe8123537b612d841c212550aca1cb0d3
Carlos Sanchez Marin <casamatechs@gmail.com>
2016-02-11 20:43:59 +0100

    Fixed Order

--- src/Order.java
+++ src/Order.java
@@ -2,7 +2,7 @@ public class Order {
 
 	private Product [] product_list;
 
-	public Order (int [] product_list){
+	public Order (Product [] product_list){
 		this.product_list = product_list;
 	}
 }
