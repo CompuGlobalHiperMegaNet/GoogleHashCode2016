aee7c1f1c0ba3d3bcfc5976c532522c7eb54f6e1
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2016-02-11 20:20:57 +0100

    Cambios sin importancia en Drone

--- src/Drone.java
+++ src/Drone.java
@@ -2,13 +2,23 @@ public class Drone {
 
 	private int payload;
 	private int max_payload;
+	
+	private int actual_turn;
+	private Location current_location;
+
+	private Order task;
 
 	private Location near_warehouse;
 
 	private Location deliver_point;
 
-	
+	public Drone (){}
 
 	
+	
+	public void load (Order task){
+		if (current_location.equals(near_warehouse)){
+			//TODO
+	}
 	
 }
---
+++ src/Simulation.java
@@ -0,0 +1,14 @@
+public class Simulation {
+
+	private int rows;
+	private int columns;
+
+	private Drone[] drone;
+
+	private int turns;
+
+	private int max_payload;
+
+	
+	
+}
