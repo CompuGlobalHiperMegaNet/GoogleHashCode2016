6f4cd039814aa550d121e51eac06b03c99cc5757
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2016-02-11 20:41:27 +0100

    Comienzo de load

--- src/Drone.java
+++ src/Drone.java
@@ -1,5 +1,7 @@
 public class Drone {
 
+	public boolean turn_used;
+	
 	private int payload;
 	private int max_payload;
 	
@@ -12,13 +14,24 @@ public class Drone {
 
 	private Location deliver_point;
 
-	public Drone (){}
-
-	
+	public Drone (int max_payload, Location current_location, Location near_warehouse){
+		this.max_payload = max_payload;
+		this.current_location = current_location;
+		this.near_warehouse = near_warehouse;
+	}
 	
 	public void load (Order task){
-		if (current_location.equals(near_warehouse)){
-			//TODO
+		if (!isOnWareHouse())
+			goToNearWareHouse();
+		//TODO
+	}
+
+	private boolean isOnWareHouse(){
+		return (current_location.equals(near_warehouse));
+	}
+	
+	private void goToNearWareHouse (){
+		//TODO
 	}
 	
 }
